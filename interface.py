5af90101950d16ca84fe12704539a90a87a219b4
fabiorx1 <binho-ramalho@hotmail.com>
2021-12-04 17:05:17 -0300

    [FIX] comentários

--- interface.py
+++ interface.py
@@ -9,19 +9,12 @@ def clickMe():
     label.configure(text= 'Texto: ' + nameEntered.get('1.0', 'end'))
  
 label = tk.Label(window, text = "Insira o texto!\n")
-# label.grid(column = 0, row = 0)
 label.pack(side=tk.BOTTOM)
  
-name = tk.StringVar()
-# nameEntered = tk.Entry(window, width = 70, textvariable = name)
 nameEntered = scrolledtext.ScrolledText(window, width=70, height=10)
-# nameEntered.grid(column = 0, row = 1)
 nameEntered.pack(side=tk.BOTTOM)
 
- 
- 
 button = tk.Button(window, text = "Click Me", command = clickMe)
-# button.grid(column= 0, row = 2)
 button.pack(side=tk.BOTTOM)
  
 window.mainloop()
