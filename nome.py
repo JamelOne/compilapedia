c87b6496251a6c8810ac9165ce2914652ab2b9f9
fabiorx1 <binho-ramalho@hotmail.com>
2021-12-04 16:58:58 -0300

    [FIX] name's regex with \. untracked

--- nome.py
+++ nome.py
@@ -13,7 +13,7 @@ maiuscula = "[A-Z|A|À|Â|Ã|É|È|Ê|Í|Ó|Ô|Õ|Ú|Ç|Ñ|-]"
 minuscula = "[a-z|á|à|â|ã|é|è|ê|í|ó|ô|õ|ú|ç|ñ|-]"
 
 nome = {0: {maiuscula: 1},
-        1: {maiuscula: 1, minuscula: 1, '.': 2},
+        1: {maiuscula: 1, minuscula: 1, '\.': 2},
         2: {' ': 0}}
 
 finais = [1,2]
