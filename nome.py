22c22b7993cf4f16f25916526347c90a3efc062c
srandre <andrelima1607@gmail.com>
2021-12-04 17:00:51 -0300

    Added remaining transition to name regex

--- nome.py
+++ nome.py
@@ -13,7 +13,7 @@ maiuscula = "[A-Z|A|À|Â|Ã|É|È|Ê|Í|Ó|Ô|Õ|Ú|Ç|Ñ|-]"
 minuscula = "[a-z|á|à|â|ã|é|è|ê|í|ó|ô|õ|ú|ç|ñ|-]"
 
 nome = {0: {maiuscula: 1},
-        1: {maiuscula: 1, minuscula: 1, '\.': 2},
+        1: {maiuscula: 1, minuscula: 1, '\.': 2, ' ': 0},
         2: {' ': 0}}
 
 finais = [1,2]
@@ -31,5 +31,5 @@ def aceitaNome(automato, inicial, finais, string):
         else: estado = ok
     return estado in finais
 
-x = aceitaNome(nome, 0, finais, 'Nome~')
+x = aceitaNome(nome, 0, finais, 'Andre Siqueira')
 print(x)
