e04e0dae8cb0d76704d70f9e9d86c4c331e41b58
JamelOne <victor.dano99@hotmail.com>
2021-12-04 18:37:31 -0300

    [ADD+FIX] Fix Name function and New Artigo functions

--- nome.py
+++ nome.py
@@ -9,27 +9,26 @@ txt = "Caio Júlio César[a] (em latim: Caius ou Gaius Iulius Caesar ou\
 #        '.': ".",
 #        ' ': " "}
 
-maiuscula = "[A-Z|A|À|Â|Ã|É|È|Ê|Í|Ó|Ô|Õ|Ú|Ç|Ñ|-]"
+maiuscula = "[A-Z|Á|À|Â|Ã|É|È|Ê|Í|Ó|Ô|Õ|Ú|Ç|Ñ|-]"
 minuscula = "[a-z|á|à|â|ã|é|è|ê|í|ó|ô|õ|ú|ç|ñ|-]"
 
-nome = {0: {maiuscula: 1},
-        1: {maiuscula: 1, minuscula: 1, '\.': 2, ' ': 0},
-        2: {' ': 0}}
-
-finais = [1,2]
 
 def transicao(estado, entrada):
     for transicao in estado.keys():
         if re.match(transicao, entrada): return estado[transicao]
     return False
 
-def aceitaNome(automato, inicial, finais, string):
-    estado = inicial
+def aceitaNome(string):
+    nome = {0: {maiuscula: 1},
+        1: {maiuscula: 1, minuscula: 1, '\.': 2, ' ': 0},
+        2: {' ': 0}}
+    finais = [1,2]
+    estado = 0
     for char in string:
-        ok = transicao(automato[estado], char)
+        ok = transicao(nome[estado], char)
         if ok == False: return False
         else: estado = ok
     return estado in finais
 
-x = aceitaNome(nome, 0, finais, 'Andre Siqueira')
+x = aceitaNome('Ándré')
 print(x)
