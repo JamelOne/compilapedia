874f8844993bcd3d124a2f90c27f8b0effabe011
fabiorx1 <binho-ramalho@hotmail.com>
2021-12-12 19:18:23 -0300

    [ADD] alterações na interface

--- interface.py
+++ interface.py
@@ -1,20 +1,55 @@
 import tkinter as tk
 from tkinter import scrolledtext
-import nome,artigo,separadores,final,adjetivo,re,automato
+import nome, artigo, separadores, final, adjetivo, automato
+import time, re
 list_regra = [nome.automato,artigo.automato,separadores.automato,
                 final.automato,adjetivo.automato]
 list_finais = [nome.finais,artigo.finais,separadores.finais,
                 final.finais,adjetivo.finais]
 texto_retorno = ["NOME", "ARTIGO", "SEPARADOR", "FINAL", "ADJETIVO"]
-def clickMe():
 
+def julioCesar():
+    julioCesar = "Caio Júlio César (em latim: Caius ou Gaius Iulius Caesar) foi um patrício, líder militar e político romano." 
+    nameEntered.setvar(julioCesar)
+def analisarLexico(lista_palavras):
+    print(lista_palavras)
+    if lista_palavras[-1][:-1]=='\n':
+        lista_palavras[-1] = lista_palavras[-1][:-1] # Removendo o \n do final.
+    
+    lista_final = []
+    for palavra in lista_palavras:
+        if(re.match(".*\,", palavra)):
+                lista_final.extend(palavra.rpartition(",")[:2])                
+        elif(re.match(".*\.", palavra)):
+                lista_final.extend(palavra.rpartition(".")[:2])
+        else:
+                lista_final.append(palavra)
+    print(lista_final)
+
+    list_tokens = list()
+    texto = ''
+    for elem in lista_final:
+        if(re.match("^(é|era|foi)$",elem)):
+            list_tokens.append({
+                    'tipo':'VERBO',
+                    'conteudo':elem})
+        else:
+            for i in range(len(list_regra)):
+                if(automato.aceita(list_regra[i], 0, list_finais[i], elem)):
+                    list_tokens.append({
+                        'tipo':texto_retorno[i],
+                        'conteudo':elem})
+                    texto+=f'"{elem}":{texto_retorno[i]}\n'
+                    break
+    label.configure(text=texto)
+    print(list_tokens)
+def clickMe():
      # ############################################
      #                   INÍCIO                  #
      #               ANALISADOR LÉXICO          #
     # ############################################
     
     texto = nameEntered.get('1.0', 'end')
-    # label.configure(text= 'Texto: ' + texto)
     texto = re.sub("[\(\[].*?[\)\]]", "", texto)
     lista_auxiliar = texto.split()
     print(lista_auxiliar)
@@ -31,16 +66,18 @@ def clickMe():
                 lista_final.append(palavra)
     print(lista_final)
     list_tokens = list()
+    texto = ''
     for elem in lista_final:
-            if(re.match("^(é|era|foi)$",elem)):
-                list_tokens.append("VERBO")
-            else:        
-                for i in range(len(list_regra)):
-                    if(automato.aceita(list_regra[i], 0, list_finais[i], elem)):
-                        list_tokens.append(texto_retorno[i])
-                        print(f'A cadeia "{elem}" foi aceita como um {texto_retorno[i]}')
-                        break
-
+        if(re.match("^(é|era|foi)$",elem)):
+            list_tokens.append("VERBO")
+        else:
+            for i in range(len(list_regra)):
+                if(automato.aceita(list_regra[i], 0, list_finais[i], elem)):
+                    list_tokens.append(texto_retorno[i])
+                    texto+=f'"{elem}":{texto_retorno[i]}\n'
+                    print('text: '+texto)
+                    break
+    label.configure(text=texto)
     print(list_tokens)
 
     # ############################################
@@ -62,5 +99,6 @@ nameEntered.pack(side=tk.BOTTOM)
 
 button = tk.Button(window, text = "Click Me", command = clickMe)
 button.pack(side=tk.BOTTOM)
- 
+
+
 window.mainloop()
