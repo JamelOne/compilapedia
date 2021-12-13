a15c5cfd53fbb3b7fbe355b1a38b0461412713ae
fabiorx1 <binho-ramalho@hotmail.com>
2021-12-13 07:34:03 -0300

    [ADD] Analisador Sintático __beta-version__

--- interface.py
+++ interface.py
@@ -8,16 +8,13 @@ list_finais = [nome.finais,artigo.finais,separadores.finais,
                 final.finais,adjetivo.finais]
 texto_retorno = ["NOME", "ARTIGO", "SEPARADOR", "FINAL", "ADJETIVO"]
 
-
-def julioCesar():
-    julioCesar = "Caio Júlio César (em latim: Caius ou Gaius Iulius Caesar) foi um patrício, líder militar e político romano." 
-    nameEntered.setvar(julioCesar)
-
 def analisarLexico(lista_palavras):
-    print(lista_palavras)
+     # Removendo o \n do final.
     if lista_palavras[-1][:-1]=='\n':
-        lista_palavras[-1] = lista_palavras[-1][:-1] # Removendo o \n do final.
+        lista_palavras[-1] = lista_palavras[-1][:-1]
     
+    
+    # Separando pontos e vírgulas de caracteres
     lista_final = []
     for palavra in lista_palavras:
         if(re.match(".*\,", palavra)):
@@ -26,8 +23,8 @@ def analisarLexico(lista_palavras):
                 lista_final.extend(palavra.rpartition(".")[:2])
         else:
                 lista_final.append(palavra)
-    print(lista_final)
 
+    # Reconhecendo à qual regra pertence cada palavra
     list_tokens = list()
     texto = ''
     for elem in lista_final:
@@ -44,50 +41,97 @@ def analisarLexico(lista_palavras):
                         'conteudo':elem})
                     texto+=f'"{elem}" : {texto_retorno[i]}\n'
                     break
+    
+    # FRONTEND
+    labelLex.configure(text='Anls. Léxico\n'+texto)
     with open('Output.txt', 'a') as f:
+        f.write('\nInício do Analisador Léxico:\n')
         f.write(texto)
-        f.close() 
-    label.configure(text=texto)
-    print(list_tokens)
+        f.close()
     return list_tokens
 
-def clickMe():
-     # ############################################
-     #                   INÍCIO                  #
-     #               ANALISADOR LÉXICO          #
-    # ############################################
+def analisadorSintatico(lista_tokens):
+    # Fazendo a junção de elementos de tipo igual
+    # que estão adjacentes, como NOME,NOME e ADJETIVO,ADJETIVO
+    lista_processada = []
+    ultimo_token = None
+    temp_token = None
+    for token in lista_tokens:
+        if ultimo_token and token['tipo']==ultimo_token['tipo']:
+            temp_token['conteudo'] += ' '+token['conteudo']
+        else:
+            if temp_token: lista_processada.append(temp_token)
+            temp_token = token
+            if(temp_token == lista_tokens[-1]): lista_processada.append(temp_token)
+        ultimo_token = token
     
-    texto = nameEntered.get('1.0', 'end')
-    with open('Output.txt', 'a') as f:
-        f.write(texto)
-        f.close() 
-    texto = re.sub("[\(\[].*?[\)\]]", "", texto)
-    lista_palavras = texto.split()
-    list_tokens=analisarLexico(lista_palavras)
-    with open('Output.txt', 'a') as f:
-        f.write('\n')
-        f.close()
-    
+    texto = ''
+    for elem in lista_processada:
+            texto+=str(elem)+'\n'
     
 
-    # ############################################
-     #                   INÍCIO                  #
-     #               ANALISADOR SINTÁTICO        #
-    # ############################################
+    # Checando se a cadeia pode ser considerada válida
+    # segundo o padrão aceito pela linguagem
+    cadeia_valida_automato = {
+        0: {"NOME": 1},
+        1: {"VERBO": 2},
+        2: {'ARTIGO': 3, "ADJETIVO": 3},
+        3: {'FINAL': 4, 'SEPARADOR': 2, 'ADJETIVO': 3},
+        4:{},
+        }
+    cadeia_valida_finais = [4]
+    res = bool()
+    if(automato.aceita(cadeia_valida_automato, 0, 
+        cadeia_valida_finais, [x['tipo'] for x in lista_processada])):
+        res = True
+        texto+='A cadeia é uma cadeia válida!\n'
+    else:
+        res = False
+        texto+='A cadeia não é uma cadeia válida!\n'
     
+    # FRONTEND
+    labelSint.configure(text='Anls. Sintático\n'+texto)
+    with open('Output.txt', 'a') as f:
+        f.write('\nInício do Analisador Sintático:\n')
+        f.write(texto)
+        f.close()
+    return res
+
+def clickMe():
+    # FRONTEND
+    texto = nameEntered.get('1.0', 'end')
+    with open('Output.txt', 'a') as f:
+        f.write('\nInício do Processamento - Texto Original:')
+        f.write(texto)
+        f.close() 
+    
+    # Removendo informações entre parênteses e afins
+    texto = re.sub("[\(\[].*?[\)\]]", "", texto)
+    
+    # Separando palavra por palavra numa lista
+    lista_palavras = texto.split()
+    lista_tokens=analisarLexico(lista_palavras)
+    acceptable = analisadorSintatico(lista_tokens)
+    with open('Output.txt', 'a') as f:
+        f.write('\n\n\n')
+        f.close()
+
 
 
 window = tk.Tk()
 window.title("Python Tkinter Text Box")
 
-label = tk.Label(window, text = "Insira o texto!\n")
-label.pack(side=tk.BOTTOM)
+labelLex = tk.Label(window, text = "Anls. Léxico\n")
+labelLex.grid(row=2, column=0)
+
+labelSint = tk.Label(window, text = "Anls. Sintático\n")
+labelSint.grid(row=2, column=1)
  
 nameEntered = scrolledtext.ScrolledText(window, width=70, height=10)
-nameEntered.pack(side=tk.BOTTOM)
+nameEntered.grid(row=1, column=0, columnspan=2)
 
 button = tk.Button(window, text = "Click Me", command = clickMe)
-button.pack(side=tk.BOTTOM)
+button.grid(row=0, column=0, columnspan=2)
 
 
 window.mainloop()
