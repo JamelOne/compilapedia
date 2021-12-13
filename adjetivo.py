c4448b01e17dd6872c12526cb8f521cfd0d9d276
fabiorx1 <binho-ramalho@hotmail.com>
2021-12-13 19:51:42 -0300

    [DEL] removes da estrutura de pastas

--- adjetivo.py
+++
@@ -1,6 +0,0 @@
-minuscula = "[a-z|á|à|â|ã|é|è|ê|í|ó|ô|õ|ú|ç|ñ|-]"
-
-automato = {0: {minuscula: 1},
-        1: {minuscula: 2},
-        2: {minuscula: 2}}
-finais = [2]
--- nome.py
+++
@@ -1,7 +0,0 @@
-maiuscula = "[A-Z|Á|À|Â|Ã|É|È|Ê|Í|Ó|Ô|Õ|Ú|Ç|Ñ|-]"
-minuscula = "[a-z|á|à|â|ã|é|è|ê|í|ó|ô|õ|ú|ç|ñ|-]"
-
-automato = {0: {maiuscula: 1},
-        1: {maiuscula: 1, minuscula: 1, '\.': 2, ' ': 0},
-        2: {' ': 0}}
-finais = [1,2]
